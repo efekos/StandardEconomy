0097d8435a21b2766a741cec4de7b9ec32eee43b
efekos <56752400+efekos@users.noreply.github.com>
2024-10-06 12:07:15 +0300

    Initial commit

---
+++ src/main/java/dev/efekos/se/StandardEconomy.java
@@ -0,0 +1,17 @@
+package dev.efekos.se;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StandardEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
