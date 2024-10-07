7295900234a413c200ed9837afc79a5d81d94fe1
efekos <56752400+efekos@users.noreply.github.com>
2024-10-07 19:33:07 +0300

    fix balance command not working without a target

--- src/main/java/dev/efekos/se/commands/BalanceCommand.java
+++ src/main/java/dev/efekos/se/commands/BalanceCommand.java
@@ -20,6 +20,7 @@ public class BalanceCommand implements BrigaiderCommand {
     @Override
     public void register(Commands commands) {
         commands.register(Commands.literal("bal")
+                        .executes(commandContext -> {if(commandContext.getSource().getSender() instanceof Player p) bal(commandContext.getSource(),p);return 0;})
                 .then(Commands.argument("target", ArgumentTypes.player()).executes(commandContext -> bal(
                         commandContext.getSource(),
                         commandContext.getArgument("target", PlayerSelectorArgumentResolver.class).resolve(commandContext.getSource()).getFirst())))
@@ -30,10 +31,10 @@ public class BalanceCommand implements BrigaiderCommand {
         EconomyProvider provider = StandardEconomy.getProvider();
         double balance = provider.getBalance(target);
         boolean self = source.getSender() instanceof Player p && p.getUniqueId().equals(target.getUniqueId());
-        format(self ? "<yellow>Your balance: <amount>" : "<yellow><target>'s balance: <amount>",
+        source.getSender().sendMessage(format(self ? "<yellow>Your balance: <amount>" : "<yellow><target>'s balance: <amount>",
                 Placeholder.component("amount", Component.text(provider.format(balance), NamedTextColor.AQUA)),
                 Placeholder.component("target", Component.text(target.getName(), NamedTextColor.AQUA).hoverEvent(target))
-        );
+        ));
         return 0;
     }
 }
